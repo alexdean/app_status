5046d3fdf4f0b2a41d6c6b85e67345cfa6a2f80e
Alex Dean <github@mostlyalex.com>
2018-02-16 15:54:24 -0600

    remove asset-related config now that we're not building assets
    [#3]

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'e97477ac3148a7b2c8db5a154bd9718e0c84a3e383dd2c74c4b37313fe6a9462303821783931c1644b66ff28144fa3e77389bc5b8311ac4371951473791e82f2'
+Dummy::Application.config.secret_key_base = 'e97477ac3148a7b2c8db5a154bd9718e0c84a3e383dd2c74c4b37313fe6a9462303821783931c1644b66ff28144fa3e77389bc5b8311ac4371951473791e82f2'
