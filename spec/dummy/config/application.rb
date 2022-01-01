89f2bda0aace6e0b41f50f5ea3d51e85ed28c688
Alex Dean <github@mostlyalex.com>
2022-01-01 12:01:50 -0600

    adjust dummy app to work with rails 7
    AppStatus::CheckCollection was not defined in spec/dummy/config/initializers/app_status.rb
    i think because of the switch to zeitwerk autoloader.
    
    wrapping checks in `Rails.application.config.to_prepare` seems to work in all rails
    versions we're testing against.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,8 +3,8 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 # require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "rails/test_unit/railtie"
+# require "action_mailer/railtie"
+# require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 require "app_status"
--- spec/dummy/config/initializers/app_status.rb
+++ spec/dummy/config/initializers/app_status.rb
@@ -1,28 +1,30 @@
-AppStatus::CheckCollection.configure do |c|
-  c.add_check('ok') do
-    [:ok, "looks good"]
+Rails.application.config.to_prepare do
+  AppStatus::CheckCollection.configure do |c|
+    c.add_check('ok') do
+      [:ok, "looks good"]
+    end
+    c.add_description 'ok', <<~EOF
+    ### OK Check
+
+    This check is always ok.
+    EOF
+
+    c.add_check('crit') do
+      [:critical, "fail"]
+    end
+    c.add_description 'crit', <<~EOF
+    ### Critical Check
+
+    This check is always critical.
+    EOF
+
+    c.add_check('no_description') do
+      [:ok, 'check has no longer description']
+    end
+
+    c.add_check('no_details') do
+      :ok
+    end
+    c.add_description('no_details', 'check only returns a status.')
   end
-  c.add_description 'ok', <<-EOF
-### OK Check
-
-This check is always ok.
-  EOF
-
-  c.add_check('crit') do
-    [:critical, "fail"]
-  end
-  c.add_description 'crit', <<-EOF
-### Critical Check
-
-This check is always critical.
-  EOF
-
-  c.add_check('no_description') do
-    [:ok, 'check has no longer description']
-  end
-
-  c.add_check('no_details') do
-    :ok
-  end
-  c.add_description('no_details', 'check only returns a status.')
 end
